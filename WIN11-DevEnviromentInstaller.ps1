9267401abab9af4af40051d74ae2ae53bfe2d521
kauê Marques <19640894+kaueMarques@users.noreply.github.com>
2023-09-07 16:36:44 -0300

    Add files via upload

---
+++ WIN11-DevEnviromentInstaller.ps1
@@ -0,0 +1,25 @@
+$langInstallComand = @(
+	
+	#Langs
+	"Python.Python.3.11", 
+	"Microsoft.OpenJDK.17",
+	"Microsoft.VCRedist.2015+.x64",
+	
+	#IDEs
+	"Microsoft.VisualStudioCode",
+	"JetBrains.IntelliJIDEA.Community",
+	"dbeaver.dbeaver",
+
+	#LOCAL DEVELOPER SERVERs
+	"ApacheFriends.Xampp.8.2 8.2.4",
+	"LeNgocKhoa.Laragon",
+
+	#CODECs
+	"VideoLAN.VLC"
+
+)
+
+foreach ($command in $langInstallComand) {
+	Write-Host "========================================"
+	winget install $command	
+}

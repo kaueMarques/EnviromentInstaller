026f3517baf4eec5d92e92b4d99a775dab2714c9
kauê Marques <19640894+kaueMarques@users.noreply.github.com>
2023-09-07 16:58:32 -0300

    feat: Linux Installer

---
+++ Linux-DevEnviromentInstaller.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+packages=(
+    
+	# Langs	
+	python3.11	
+	openjdk-17-jdk
+	
+	# IDEs
+	code	
+	intellij-idea-community
+	dbeaver-ce
+
+	# Codecs
+	vlc
+
+	# Corp Communication
+
+)
+
+for package in "${packages[@]}"; do
+    echo "========================================"
+    sudo apt-get install -y "$package"
+done

7d2077045883f9a8f77969b0d4d2e106ad4a7e95
kauê Marques <19640894+kaueMarques@users.noreply.github.com>
2023-09-09 19:20:51 +0000

    refactor

--- WIN11-DevEnviromentInstaller.ps1
+++ WIN11-DevEnviromentInstaller.ps1
@@ -1,20 +1,20 @@
-function List-AppsInCategory($category) {
-    $jsonContent.$category | ForEach-Object { Write-Host "-  $_" }
-}
+$jsonAppListURL = "https://raw.githubusercontent.com/kaueMarques/EnviromentInstaller/master/win-apps.json"
+$jsonAppContent = Invoke-RestMethod -Uri $jsonAppListURL
 
+function List-AppsInCategory($category) {
+    $jsonAppContent.$category | ForEach-Object { Write-Host "-  $_" }
+}
 function Install-AppsInCategory($category) {
     Write-Host "Installing apps in category '$category'..."
-    $jsonContent.$category | ForEach-Object {
+    $jsonAppContent.$category | ForEach-Object {
         Write-Host "Installing $_..." -NoNewline
         Start-Process "winget" -ArgumentList "install $_" -Wait
-        Write-Host " done."
     }
 }
-
 function List-Categories {
     Clear-Host
     Write-Host "Available categories:"
-    $jsonContent.PSObject.Properties.Name | ForEach-Object {
+    $jsonAppContent.PSObject.Properties.Name | ForEach-Object {
         $category = $_
         Write-Host ("{0} - {1}:" -f $i, $category)
         List-AppsInCategory $category
@@ -24,7 +24,6 @@ function List-Categories {
     Write-Host "0 - Install all"
     Write-Host "x - Exit"
 }
-
 function Install-Selected-Categories {
     $selectedCategories = Read-Host "confirm with Categorie number or press 'x' to return" -split ','
     
@@ -32,19 +31,16 @@ function Install-Selected-Categories {
         $categoryNumber = [int]$_
         if ($categoryNumber -eq 0) { Install-All-Apps }
         elseif ($categoryNumber -eq $null) { return }
-        elseif ($jsonContent.PSObject.Properties.Name[$categoryNumber - 1]) { Install-AppsInCategory $jsonContent.PSObject.Properties.Name[$categoryNumber - 1] }
+        elseif ($jsonAppContent.PSObject.Properties.Name[$categoryNumber - 1]) { Install-AppsInCategory $jsonAppContent.PSObject.Properties.Name[$categoryNumber - 1] }
         else { Write-Host "Invalid category number: $_. Please try again." }
     }
 }
-
 function Install-All-Apps {
     Write-Host "Installing all apps in all categories..."
-    $jsonContent.PSObject.Properties.Name | ForEach-Object { Install-AppsInCategory $_ }
+    $jsonAppContent.PSObject.Properties.Name | ForEach-Object { Install-AppsInCategory $_ }
 }
-
 function Show-Menu {
     $i = 1
-    while ($true) {
         List-Categories
 		Write-Host ""
         $choice = Read-Host "choice"
@@ -52,10 +48,5 @@ function Show-Menu {
             "x" { exit }
             default { Install-Selected-Categories }
         }
-    }
 }
-
-$jsonUrl = "https://raw.githubusercontent.com/kaueMarques/EnviromentInstaller/master/win-apps.json"
-$jsonContent = Invoke-RestMethod -Uri $jsonUrl
-
 Show-Menu

b5b5abefae1a8fb5b4101151da38fcfb151bf968
kauê Marques <19640894+kaueMarques@users.noreply.github.com>
2023-09-10 19:57:48 +0000

    refactor: use json and CLI gui in Mac and Linux

--- scripts/Linux-DevEnviromentInstaller.sh
+++ scripts/Linux-DevEnviromentInstaller.sh
@@ -1,63 +1,114 @@
 #!/bin/bash
 
-langInstallComand=(
-    # Dev Tools
-    "git"
-    "virtualbox"
+jsonAppListURL="https://raw.githubusercontent.com/kaueMarques/EnviromentInstaller/master/repos/linux-apps.json"
+jsonAppContent=$(curl -s "$jsonAppListURL")
 
-    # Browser
-    "google-chrome-stable"
-
-    # Langs
-    "python3.11"
-    "openjdk-17-jdk"
-    "wine32"
-    "nodejs"
-    "ruby-full"
-
-    # IDEs
-    "notepad-plus-plus"
-    "code"
-    "openjdk-17-jdk"
-    "dbeaver-ce"
-
-    # LOCAL DEVELOPER SERVERs
-    "apache2"
-    "laragon"
-    "docker"
-    "podman"
-
-    # CODECs
-    "vlc"
-
-    # Popular Corp Communication
-    "slack-desktop"
-    "teams"
-
-    # Others Communication
-    "discord"
-  )
-
-
-if [ -f /etc/os-release ]; then
-  source /etc/os-release
-  case $ID in
-    "centos" | "fedora")
-      package_manager="yum"
-      ;;
-    "ubuntu" | "debian")
-      package_manager="apt"
-      ;;
-    *)
-      echo "Error: Unable to determine the package management system."
-      exit 1
-      ;;
-  esac
-
-  for command in "${langInstallComand[@]}"; do
-    sudo $package_manager install -y "$command"
-  done
+if [ -f "/etc/os-release" ]; then
+    source /etc/os-release
+    case "$ID" in
+        ubuntu|debian)
+            package_manager="apt"
+            ;;
+        fedora)
+            package_manager="dnf"
+            ;;
+        rhel|centos|amzn)
+            package_manager="yum"
+            ;;
+        suse|opensuse-leap|opensuse-tumbleweed)
+            package_manager="zypper"
+            ;;
+        *)
+            echo "Unsupported distribution: $ID"
+            exit 1
+            ;;
+    esac
 else
-  echo "Error: File /etc/os-release not found. Unable to identify the package management system."
-  exit 1
+    echo "Unable to detect the Linux distribution."
+    exit 1
 fi
+
+echo "Detected distribution: $ID"
+echo "Recommended package manager: $package_manager"
+
+List-AppsInCategory() {
+    category="$1"
+    echo "$jsonAppContent" | jq -r ".\"$category\"[]"
+}
+
+Install-AppsInCategory() {
+    category="$1"
+    echo "Installing apps in category '$category'..."
+    apps=($(echo "$jsonAppContent" | jq -r ".\"$category\"[]"))
+    for app in "${apps[@]}"; do
+        echo "Installing $app..."
+        
+        case "$package_manager" in
+            "apt")
+                sudo apt-get install -y "$app"
+                ;;
+            "dnf")
+                sudo dnf install -y "$app"
+                ;;
+            "yum")
+                sudo yum install -y "$app"
+                ;;
+            "zypper")
+                sudo zypper install -y "$app"
+                ;;
+        esac
+    done
+}
+
+List-Categories() {
+    clear
+    echo "Available categories:"
+    i=1
+    categories=($(echo "$jsonAppContent" | jq -r 'keys[]'))
+    for category in "${categories[@]}"; do
+        echo "$i - $category:"
+        List-AppsInCategory "$category"
+        echo ""
+        ((i++))
+    done
+    echo "0 - Install all"
+    echo "x - Exit"
+}
+
+Install-Selected-Categories() {
+    read -p "Confirm with Category number or press 'x' to return: " selectedCategories
+
+    for categoryNumber in $(echo "$selectedCategories" | tr ',' ' '); do
+        if [[ "$categoryNumber" -eq 0 ]]; then
+            Install-All-Apps
+        elif [[ "$categoryNumber" == "x" ]]; then
+            return
+        elif [[ "${categories[$categoryNumber - 1]}" ]]; then
+            Install-AppsInCategory "${categories[$categoryNumber - 1]}"
+        else
+            echo "Invalid category number: $categoryNumber. Please try again."
+        fi
+    done
+}
+
+Install-All-Apps() {
+    echo "Installing all apps in all categories..."
+    for category in "${categories[@]}"; do
+        Install-AppsInCategory "$category"
+    done
+}
+
+Show-Menu() {
+    i=1
+    while true; do
+        List-Categories
+        echo ""
+        read -p "Enter your choice: " choice
+        case "$choice" in
+            "x") exit ;;
+            *) Install-Selected-Categories ;;
+        esac
+    done
+}
+
+Show-Menu
--- scripts/MacOS-DevEnviromentInstaller.sh
+++ scripts/MacOS-DevEnviromentInstaller.sh
@@ -1,39 +1,87 @@
 #!/bin/bash
 
-langInstallComand=(
-	# Dev Tools
-	"git"
- 	"virtualbox"
+jsonAppListURL="https://raw.githubusercontent.com/kaueMarques/EnviromentInstaller/master/repos/mac-apps.json"
+jsonAppContent=$(curl -s "$jsonAppListURL")
 
-	# Browser
-	"google-chrome"
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    package_manager="brew"
+else
+    echo "Unsupported operating system: $OSTYPE"
+    exit 1
+fi
 
-	# Langs
-	"python@3.11"
-	"openjdk@17"
-	"node"
-	"ruby"
+echo "Detected operating system: macOS"
+echo "Recommended package manager: $package_manager"
 
-	# IDEs
-	"visual-studio-code"
-	"dbeaver-community"
+List-AppsInCategory() {
+    category="$1"
+    echo "$jsonAppContent" | jq -r ".\"$category\"[]"
+}
 
-	# LOCAL DEVELOPER SERVERs
-	"docker"
- 	"podman"
+Install-AppsInCategory() {
+    category="$1"
+    echo "Installing apps in category '$category'..."
+    apps=($(echo "$jsonAppContent" | jq -r ".\"$category\"[]"))
+    for app in "${apps[@]}"; do
+        echo "Installing $app..."
+        
+        case "$package_manager" in
+            "brew")
+                brew install "$app"
+                ;;
+        esac
+    done
+}
 
-	# CODECs
-	"vlc"
+List-Categories() {
+    clear
+    echo "Available categories:"
+    i=1
+    categories=($(echo "$jsonAppContent" | jq -r 'keys[]'))
+    for category in "${categories[@]}"; do
+        echo "$i - $category:"
+        List-AppsInCategory "$category"
+        echo ""
+        ((i++))
+    done
+    echo "0 - Install all"
+    echo "x - Exit"
+}
 
-	# Popular Corp Communication
-	"slack"
-	"microsoft-teams"
+Install-Selected-Categories() {
+    read -p "Confirm with Category number or press 'x' to return: " selectedCategories
 
-	# Others Communication
-	"discord"
-)
+    for categoryNumber in $(echo "$selectedCategories" | tr ',' ' '); do
+        if [[ "$categoryNumber" -eq 0 ]]; then
+            Install-All-Apps
+        elif [[ "$categoryNumber" == "x" ]]; then
+            return
+        elif [[ "${categories[$categoryNumber - 1]}" ]]; then
+            Install-AppsInCategory "${categories[$categoryNumber - 1]}"
+        else
+            echo "Invalid category number: $categoryNumber. Please try again."
+        fi
+    done
+}
 
-for command in "${langInstallComand[@]}"; do
-	echo "========================================"
-	brew install "$command"
-done
+Install-All-Apps() {
+    echo "Installing all apps in all categories..."
+    for category in "${categories[@]}"; do
+        Install-AppsInCategory "$category"
+    done
+}
+
+Show-Menu() {
+    i=1
+    while true; do
+        List-Categories
+        echo ""
+        read -p "Enter your choice: " choice
+        case "$choice" in
+            "x") exit ;;
+            *) Install-Selected-Categories ;;
+        esac
+    done
+}
+
+Show-Menu
